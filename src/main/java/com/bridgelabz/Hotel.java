4a4f68389ba983940ae26e64bae38a74c476c256
Kiran <makamkiran95@gmail.com>
2021-11-18 09:29:00 +0530

    Refactored : Checking the hotel with name whether its present in the list

--- src/main/java/com/bridgelabz/Hotel.java
+++ src/main/java/com/bridgelabz/Hotel.java
@@ -1,11 +1,20 @@
 package com.bridgelabz;
 
+/**
+ * this class is for hotel objects
+ */
 public class Hotel {
-
+    //class level variables
     String hotelName;
     int hotelRating;
 
+    /**
+     * this constructor is to create objects
+     * @param hotelName
+     * @param hotelRating
+     */
     Hotel(String hotelName, int hotelRating) {
+        //Assigning values to class level variables
         this.hotelName = hotelName;
         this.hotelRating = hotelRating;
     }
--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -7,17 +7,25 @@ public class HotelReservationSystem {
     List<Hotel> hotelsList = new LinkedList<>();
 
     /**
-     * adding hotels to list
+     * adding hotels to list and checking whether it is present or not
      * @param str
-     * @return
+     * @return true or false
      */
     public boolean addingHotelsToList(String str){
+        //creating new hotels
         Hotel hotel1 = new Hotel("LakeWood",3);
         Hotel hotel2 = new Hotel("BridgeWood",4);
         Hotel hotel3 = new Hotel("RidgeWood",5);
-        hotelsList.add(hotel1);
-        hotelsList.add(hotel2);
-        hotelsList.add(hotel3);
+        //Adding hotels in to the list
+        try {
+            hotelsList.add(hotel1);
+            hotelsList.add(hotel2);
+            hotelsList.add(hotel3);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        //checking whether the hotel is present or not
         return hotelsList.stream().filter(hotel -> hotel.hotelName == str ).findFirst().isPresent();
     }
 
